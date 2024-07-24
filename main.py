107273046381dd3aa065fab07c4f85ced29bc9ac
lukasha11 <lukas11ha11@gmail.com>
2024-07-24 23:57:58 +0200

    zliczanie

--- main.py
+++ main.py
@@ -1,28 +1,34 @@
 import random
 
-print("smth")
-
-plansza = []
-wymiar = 5
-
-for i in range(wymiar):
-    wiersz = []
-    for j in range(wymiar):
-        wiersz.append(0)
-    plansza.append(wiersz)
-
-for i in range(wymiar):
-    print(plansza[i])
+def wyswietl_plansze(tab):
+    for i in range(wymiar):
+        print(tab[i])
 
 def wypelnij_bombami(tab, wymiar):
     licznik = 0
-    losowane = random.sample(range(1, 26), 5)
+    losowane = random.sample(range(1, wymiar*wymiar), wymiar)
     for i in range(wymiar):
         for j in range(wymiar):
             if licznik in losowane:
                 tab[i][j] = "*"
             licznik += 1
 
-wypelnij_bombami(plansza, wymiar)
+def zlicz_sasiedztwo(tab):
+    for i in range(1, wymiar - 1):
+        for j in range(1, wymiar - 1):
+            if tab[i][j] == '*':
+
+
+
+plansza = []
+wymiar = 5
 for i in range(wymiar):
-    print(plansza[i])
+    wiersz = []
+    for j in range(wymiar):
+        wiersz.append(0)
+    plansza.append(wiersz)
+
+
+wypelnij_bombami(plansza, wymiar)
+
+wyswietl_plansze(plansza)
