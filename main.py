5f9309cb6a5c42e5cf55bda3a308670faede164d
lukasha11 <lukas11ha11@gmail.com>
2024-07-26 12:22:42 +0200

    program działa, bez odkrywania pystych

--- main.py
+++ main.py
@@ -14,34 +14,33 @@ def wypelnij_bombami(tab):
     for i in range(len(tab)):
         for j in range(len(tab)):
             if licznik in losowane:
-                tab[i][j] = "*"
+                tab[i][j].dodaj_bombe()
             licznik += 1
 
 
 def zlicz_sasiedztwo(tab):
     for i in range(len(tab)):
         for j in range(len(tab)):
-            if tab[i][j] == '*':
+            if tab[i][j].czy_bomba == 1:
                 try:
-                    if isinstance(tab[i - 1][j - 1], int):
-                        tab[i - 1][j - 1] += 1
-                    if isinstance(tab[i - 1][j], int):
-                        tab[i - 1][j] += 1
-                    if isinstance(tab[i - 1][j + 1], int):
-                        tab[i - 1][j + 1] += 1
-                    if isinstance(tab[i][j - 1], int):
-                        tab[i][j - 1] += 1
-                    if isinstance(tab[i][j + 1], int):
-                        tab[i][j + 1] += 1
-                    if isinstance(tab[i + 1][j - 1], int):
-                        tab[i + 1][j - 1] += 1
-                    if isinstance(tab[i + 1][j], int):
-                        tab[i + 1][j] += 1
-                    if isinstance(tab[i + 1][j + 1], int):
-                        tab[i + 1][j + 1] += 1
+                    if not tab[i - 1][j - 1].czy_bomba:
+                        tab[i - 1][j - 1].dodaj_wartosc()
+                    if not tab[i - 1][j].czy_bomba:
+                        tab[i - 1][j].dodaj_wartosc()
+                    if not tab[i - 1][j + 1].czy_bomba:
+                        tab[i - 1][j + 1].dodaj_wartosc()
+                    if not tab[i][j - 1].czy_bomba:
+                        tab[i][j - 1].dodaj_wartosc()
+                    if not tab[i][j + 1].czy_bomba:
+                        tab[i][j + 1].dodaj_wartosc()
+                    if not tab[i + 1][j - 1].czy_bomba:
+                        tab[i + 1][j - 1].dodaj_wartosc()
+                    if not tab[i + 1][j].czy_bomba:
+                        tab[i + 1][j].dodaj_wartosc()
+                    if not tab[i + 1][j + 1].czy_bomba:
+                        tab[i + 1][j + 1].dodaj_wartosc()
                 except IndexError:
                     continue
-
 
 def tworz_plansze(wymiar):
     plansza = []
@@ -52,6 +51,9 @@ def tworz_plansze(wymiar):
         plansza.append(wiersz)
     return plansza
 
+# def pokaz_zera(tab):
+#     for i in range(len(tab)):
+#         for j in range(len(tab)):
 
 class Pole:
     def __init__(self, czy_odkryta, wartosc, czy_bomba):
@@ -62,8 +64,40 @@ class Pole:
     def dodaj_bombe(self):
         self.czy_bomba = 1
 
+    def dodaj_wartosc(self):
+        self.wartosc += 1
 
-plansza = tworz_plansze(7)
-wypelnij_bombami(plansza)
-zlicz_sasiedztwo(plansza)
-wyswietl_plansze(plansza)
+    def odkryj(self):
+        self.czy_odkryta = 1
+
+    def __str__(self):
+        # jesli nieodkryta to wyswietlam []
+        if not self.czy_odkryta:
+            return "[]"
+        if self.czy_odkryta and self.czy_bomba:
+            return "*"
+        return str(self.wartosc)
+
+
+def gra():
+    plansza = tworz_plansze(7)
+    wypelnij_bombami(plansza)
+    zlicz_sasiedztwo(plansza)
+    print("podaj wspolrzedne -> od 0,0 do 6,6")
+
+    while (True):
+        a = int(input("wsp a = "))
+        b = int(input("wsp b = "))
+        if plansza[a][b].czy_odkryta:
+            print("juz odkryta")
+            continue
+        plansza[a][b].odkryj()
+        wyswietl_plansze(plansza)
+
+        if plansza[a][b].czy_bomba:
+            print("przegrales")
+            break
+
+
+
+gra()
