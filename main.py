d1235e6145900a7bf639b8385b5d779f5866d8c5
lukasha11 <lukas11ha11@gmail.com>
2024-07-26 09:34:05 +0200

    dodanie klasy Pole

--- main.py
+++ main.py
@@ -17,36 +17,30 @@ def wypelnij_bombami(tab):
                 tab[i][j] = "*"
             licznik += 1
 
-# def spr_sasiada(tab,i,j):
-#     if tab[i - 1][j - 1]:
-#
-#         tab[i - 1][j]
-#         tab[i - 1][j + 1]
-#         tab[i][j - 1]
-#         tab[i][j + 1]
-#         tab[i + 1][j - 1]
-#         tab[i + 1][j]
-#         tab[i + 1][j + 1]
+
 def zlicz_sasiedztwo(tab):
-    for i in range(1, len(tab) - 1):
-        for j in range(1, len(tab) - 1):
+    for i in range(len(tab)):
+        for j in range(len(tab)):
             if tab[i][j] == '*':
-                if isinstance(tab[i - 1][j - 1], int):
-                    tab[i - 1][j - 1] += 1
-                if isinstance(tab[i - 1][j], int):
-                    tab[i - 1][j] += 1
-                if isinstance(tab[i - 1][j + 1], int):
-                    tab[i - 1][j + 1] += 1
-                if isinstance(tab[i][j - 1], int):
-                    tab[i][j - 1] += 1
-                if isinstance(tab[i][j + 1], int):
-                    tab[i][j + 1] += 1
-                if isinstance(tab[i + 1][j - 1], int):
-                    tab[i + 1][j - 1] += 1
-                if isinstance(tab[i + 1][j], int):
-                    tab[i + 1][j] += 1
-                if isinstance(tab[i + 1][j + 1], int):
-                    tab[i + 1][j + 1] += 1
+                try:
+                    if isinstance(tab[i - 1][j - 1], int):
+                        tab[i - 1][j - 1] += 1
+                    if isinstance(tab[i - 1][j], int):
+                        tab[i - 1][j] += 1
+                    if isinstance(tab[i - 1][j + 1], int):
+                        tab[i - 1][j + 1] += 1
+                    if isinstance(tab[i][j - 1], int):
+                        tab[i][j - 1] += 1
+                    if isinstance(tab[i][j + 1], int):
+                        tab[i][j + 1] += 1
+                    if isinstance(tab[i + 1][j - 1], int):
+                        tab[i + 1][j - 1] += 1
+                    if isinstance(tab[i + 1][j], int):
+                        tab[i + 1][j] += 1
+                    if isinstance(tab[i + 1][j + 1], int):
+                        tab[i + 1][j + 1] += 1
+                except IndexError:
+                    continue
 
 
 def tworz_plansze(wymiar):
@@ -54,14 +48,22 @@ def tworz_plansze(wymiar):
     for i in range(wymiar):
         wiersz = []
         for j in range(wymiar):
-            wiersz.append(0)
+            wiersz.append(Pole(0, 0, 0))
         plansza.append(wiersz)
     return plansza
 
 
-plansza = tworz_plansze(5)
+class Pole:
+    def __init__(self, czy_odkryta, wartosc, czy_bomba):
+        self.czy_odkryta = czy_odkryta
+        self.wartosc = wartosc
+        self.czy_bomba = czy_bomba
+
+    def dodaj_bombe(self):
+        self.czy_bomba = 1
+
+
+plansza = tworz_plansze(7)
 wypelnij_bombami(plansza)
-wyswietl_plansze(plansza)
 zlicz_sasiedztwo(plansza)
-print()
 wyswietl_plansze(plansza)
